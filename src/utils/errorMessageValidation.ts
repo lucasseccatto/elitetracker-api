2ec268560ef65d8f3a359c6fb9b164580a0f2921
Lucas Seccatto <lucasseccatto1@gmail.com>
2024-11-28 12:51:40 -0300

    Elitetracker API

---
+++ src/utils/errorMessageValidation.ts
@@ -0,0 +1,9 @@
+import { ZodIssue } from "zod";
+
+export function ErrorMessageValidation(issues: ZodIssue[]): string[] {
+  const errors = issues.map(
+    (item) => `${item.path.join(".")}: ${item.message}`
+  );
+
+  return errors;
+}
